ab1f99d94196a5181250064c98b527bd43f64300
arim83 <jorammercado@pursuit.org>
2024-12-02 15:46:58 -0500

    update footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import logo from "../assets/pursuit.png"
 import "./Footer.scss"
 
 const Footer = () => {
     return (
         <footer className="footer text-white py-4">
-            <p className="footer__text">&copy; 2024 Budgeting and Investment Recommendation Application. All rights reserved.</p>
+            <p className="footer__text">&copy; 2024 Budgeting and Investment Recommendation Application - Test. All rights reserved.</p>
             <div className="footer__social-links">
+                <a href="https://www.pursuit.org" target="_blank" rel="noopener noreferrer" className="footer__social-links__link ">
+                    <img src={logo} />
+                </a>
                 <a href="https://www.linkedin.com/in/jorammercado" target="_blank" rel="noopener noreferrer" className="footer__social-links__link">
                     <FontAwesomeIcon icon={faLinkedin} />
                 </a>
