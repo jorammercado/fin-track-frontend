c693b482274fe91fbd6ae1dc4b2323eddbb42de6
arim83 <jorammercado@pursuit.org>
2024-12-01 20:29:56 -0500

    add social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import "./Footer.scss" 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import "./Footer.scss"
 
 const Footer = () => {
     return (
-        <footer className="footer text-white text-center py-4">
-            <p> &copy; 2024 Budgeting and Investment Recommendation Application. All rights reserved.</p>
+        <footer className="footer text-white py-4">
+            <p className="footer__text">&copy; 2024 Budgeting and Investment Recommendation Application. All rights reserved.</p>
+            <div className="footer__social-links">
+                <a href="https://www.linkedin.com/in/jorammercado" target="_blank" rel="noopener noreferrer" className="footer__social-links__link">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                <a href="https://github.com/jorammercado" target="_blank" rel="noopener noreferrer" className="footer__social-links__link">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+            </div>
         </footer>
     )
 }
