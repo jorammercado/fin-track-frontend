9d4c8e82d641fd47a63e64cd29104b415a46891b
arim83 <jorammercado@pursuit.org>
2024-12-13 19:02:53 -0500

    add financial news with data from api

--- src/pages/MarketNews.jsx
+++ src/pages/MarketNews.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./MarketNews.scss"
 
-const MarketNews = () => {
+const API = import.meta.env.VITE_FINNHUB_API_KEY
+
+const FinancialNews = () => {
+    const [news, setNews] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const fetchFinancialNews = async () => {
+            try {
+                const response = await fetch(`https://finnhub.io/api/v1/news?category=general&token=${API}`)
+                if (!response?.ok) {
+                    throw new Error(`Error: ${response?.statusText}`)
+                }
+                const data = await response?.json()
+                setNews(data)
+                setLoading(false)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchFinancialNews()
+    }, [])
+
+    if (loading) return <p>Loading news...</p>
+
     return (
-        <div>
-            <h1>Market news</h1>
+        <div className="news" >
+            <div className="news__container" >
+                {news?.map((item, index) => (
+                    <div className="news__container__story"
+                        key={index}
+                        onClick={() => window.open(item?.url, '_blank', 'noopener, noreferrer')}
+                    >
+                        <div className="news__container__story__text">
+                            <div className="headline" >{item?.headline}</div>
+                            <div className="summary" >{item?.summary}</div>
+                        </div>
+                        <div>
+                            <img src={item?.image}
+                                height={
+                                    item?.image?.includes('market_watch_logo.png') ? "20px" : "55px"}
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
 
-export default MarketNews
+export default FinancialNews
